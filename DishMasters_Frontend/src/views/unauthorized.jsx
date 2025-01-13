c77b60bcc85e0d60d3bada86ce1caa4caf0e4c5f
Ubuntu <ubuntu@ip-172-31-24-155.ec2.internal>
2025-01-13 12:59:09 +0000

    Unauthorized image change

--- DishMasters_Frontend/src/views/unauthorized.jsx
+++ DishMasters_Frontend/src/views/unauthorized.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 export const Unauthorized = () => {
   return (
     <div className="text-center mt-5">
-        <div className='flex justify-center items-center flex-row gap-4'> 
+      <div className='flex justify-center items-center flex-row gap-4'>
         <h1>403 - Unauthorized</h1>
-        <img src="img/DishyAngry.png" className='w-16 h-16 object-cover' alt="" />
-        </div>
-      
+        <img src="img/DishyMad.png" className='w-16 h-16 object-cover' alt="" />
+      </div>
+
       <p>You are not authorized to access this page.</p>
       <button>
         <Link to="/" className="text-[#FFBD59] hover:text-[#ff9f3d]">Back to Home</Link>
